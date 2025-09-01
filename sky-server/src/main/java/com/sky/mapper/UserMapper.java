977700426cb86ca8b073db462d31a10a549d2bdb
Kuang <Kaung@qq.com>
2025-09-01 14:48:19 +0800

    add

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import static com.sky.enumeration.OperationType.INSERT;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+//    @AutoFill(INSERT)
+    void insertUser(User user);
+}
